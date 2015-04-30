98c0b00c0701af0304e32dff06374e9f0ace1aa6
himkt <himkt@klis.tsukuba.ac.jp>
2015-04-30 13:07:26 +0900

    update nowcontroller (disable post tweet when it hasn't kcode)

--- app/controllers/now_controller.rb
+++ app/controllers/now_controller.rb
@@ -1,7 +1,7 @@
 class NowController < ApplicationController
   def index
     @post = Now.new
-    @posts = Now.all
+    @posts = Now.all.reverse
     param = params
     tweet = param["now"]["text"] if param["now"]
     subject_kcode = 0
@@ -9,10 +9,16 @@ class NowController < ApplicationController
       update(tweet, subject_kcode)
     end
   end
+
+  def brows(subject_kcode)
+    
+  end
   
 	def update(tweet,subject_kcode='0')
-    Now.create(:subject_kcode=>subject_kcode, :text=>tweet)
-    render action: :index
+    if subject_kcode != '0'
+      Now.create(:subject_kcode=>subject_kcode, :text=>tweet)
+      render action: :index
+    end
 	end
 
 	def delete(id=nil)
