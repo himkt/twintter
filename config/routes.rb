a4cc10d15af2f0690e50f48c8a94d4c19039078f
himkt <himkt@klis.tsukuba.ac.jp>
2015-05-02 00:30:39 +0900

    merge shopetan-merge(search/search|detail) and fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
   #   get 'search/detail/:kcode' => 'search#detail/:kcode' 
   # end
   get 'search/detail/:kcode' => 'search#detail#:kcode' 
+  post 'search/detail/:kcode' => 'search#detail#:kcode' 
+
+  post 'now/index' => 'now/index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
