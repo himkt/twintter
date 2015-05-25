556165272edc4bd3c9265f4a4744a75916029daf
himkt <himkt@klis.tsukuba.ac.jp>
2015-05-25 16:19:08 +0900

    add stemmify in Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,52 @@ kcodes = Subject.all
   kcode = kcodes.sample(1)[0].kcode#[:kcode]
   Now.create({:subject_kcode=>kcode, :text=>comments.sample(1)[0]})
 end
+
+require 'stemmify'
+
+dic = Hash.new
+
+if ARGV[0] == 'ja'
+
+  puts 'make a model'
+  File.foreach('data/pn_ja.dic') do |line|
+    if item = line.chomp.split(/\:/)
+      dic[item[0]] = item[3]
+    end
+  end
+  puts 'done'
+
+
+  puts 'make a database'
+  db = SQLite3::Database.new("data/model.db")
+  sql = <<-SQL
+  CREATE TABLE models (
+  word TEXT ,
+  value REAL
+  );
+  SQL
+  db.execute(sql)
+  puts 'done'
+
+
+  puts 'insert a value into a database'
+  dic.each do |word,value|
+    sql = <<-SQL
+    INSERT INTO models (word, value) VALUES('#{word}', #{value});
+    SQL
+    db.execute(sql)
+  end
+  puts 'done'
+
+elsif ARGV[0] == 'en'
+  puts 'make a model'
+  File.foreach('data/pn_en.dic') do |line|
+    if item = line.chomp.split(/\:/)
+      dic[item[0]] = item[2]
+    end
+  end
+  puts 'done'
+
+  db = SQLite3::Database.new("data/model.db")
+  puts 'insert a value into a database'
+  dic.each do |word,value|
