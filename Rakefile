127c223f752fddbe99368547c29cdcecf5774b20
himkt <himkt@klis.tsukuba.ac.jp>
2015-05-26 07:28:29 +0900

    update seeds.rb and Rakefile

--- Rakefile
+++ Rakefile
@@ -4,3 +4,13 @@
 require File.expand_path('../config/application', __FILE__)
 
 Rails.application.load_tasks
+
+task :build do
+  sh "rm -rf vendor/model"
+  sh "mkdir vendor/model"
+  sh "curl http://www.lr.pi.titech.ac.jp/%7Etakamura/pubs/pn_ja.dic >> vendor/model/pn_ja.dic"
+  sh "curl http://www.lr.pi.titech.ac.jp/%7Etakamura/pubs/pn_en.dic >> vendor/model/pn_en.dic"
+  sh "nkf -w vendor/model/pn_ja.dic > vendor/model/pn_ja.dic.conv"
+  sh "rm vendor/model/pn_ja.dic"
+  sh "mv vendor/model/pn_ja.dic.conv vendor/model/pn_ja.dic"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,32 +10,13 @@
 # rails g model Subject kcode:string unit:float kname:string grade:string semester:string time:string location:string lecturer:text summary:text note:text credit:string condition:text alternative:string
 
 =begin
+puts 'seed subjects...'
 File.foreach('vendor/subject/cutted_subjects.tsv') do |line|
   next if $. == 1
   items = line.split(/\t/)
   Subject.create({:kcode=>items[0].sub(/\.0$/,''), :kname=>items[1], :unit=>items[2], :grade=>items[3], :semester=>items[4], :time=>items[5], :location=>items[6], :lecturer=>items[7], :summary=>items[8], :note=>items[9], :credit=>items[10], :condition=>items[11], :alternative=>items[12]})
 end
-=end
-
-=begin
-comments = [
-"課題出た？",
-"なんか眠くなってきた",
-"完全に理解した",
-"この授業楽しいな",
-"全く理解できない",
-"早く終われ頼む",
-"楽しい。わかってきた",
-"こんなに簡単だったのか。"
-]
-
-kcodes = Subject.all
-
-100000.times do
-  kcode = kcodes.sample(1)[0].kcode#[:kcode]
-  Now.create({:subject_kcode=>kcode, :text=>comments.sample(1)[0]})
-end
-=end
+puts 'done'
 
 
 dic = Hash.new
@@ -52,11 +33,36 @@ File.foreach('vendor/model/pn_en.dic') do |line|
   end
 end
 puts 'done'
+=end
 
-p dic.size
+comments = [
+"課題出た？",
+"なんか眠くなってきた",
+"完全に理解した",
+"この授業楽しいな",
+"全く理解できない",
+"早く終われ頼む",
+"楽しい。わかってきた",
+"こんなに簡単だったのか。"
+]
 
-puts 'seed data'
-dic.each do |word,value|
-  Model.create({:word=>word, :value=>value})
+kcodes = Subject.all
+
+puts 'seed nows'
+100000.times do
+  sum = 0
+  kcode = kcodes.sample(1)[0].kcode#[:kcode]
+  tweet = comments.sample(1)[0]
+  nm = Natto::MeCab.new
+  nm.parse(tweet).split(/\n/).each do |line|
+    elem = line.split(/\t/)
+    break if elem[0] == 'EOS' 
+    word = (elem[1].split(/\,/)[6] == '*') ? elem[0].stem : elem[1].split(/\,/)[6]
+    value = Model.where("word = '#{word}'")[0]
+    sum += value.value if value
+  end
+  e = Math.exp(1)
+  score = (e ** sum - e** (-1 * sum)) / (e ** sum + e** (-1 * sum))
+  Now.create(:subject_kcode=>kcode, :text=>tweet ,:deleted=>0,:score=>score)
 end
 puts 'done'
